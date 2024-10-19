637985796f1408f226258121fe05b97afeafdb63
Rayya <danil.asd24@gmail.com>
2024-10-19 20:22:13 +0300

    Resolve "Setup swagger for API" (#4)
    * delete useless comment
    
    * add docs dir to .gitignore
    
    * update dependencies
    
    * - add docs title/description
    - add swagger path.
    
    * add swagger path.
    
    * add documentation for /ping

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,7 @@
-// cmd/api/main.go
 package main
 
 import (
+	_ "bizarre-vpn-api/docs"
 	"bizarre-vpn-api/internal/api/routes"
 	"bizarre-vpn-api/pkg/logger"
 	"github.com/joho/godotenv"
@@ -9,6 +9,11 @@ import (
 	"os"
 )
 
+// @title BizarreVPN API
+// @version 0.0.1
+// @description API for BizarreVPN project.
+// @host localhost:8080
+// @BasePath /
 func main() {
 	if err := godotenv.Load(); err != nil {
 		logger.Error.Printf("Error loading .env : %v", err)
@@ -20,7 +25,13 @@ func main() {
 		return
 	}
 
-	r := routes.SetupRouter()
+	swaggerPath := os.Getenv("SWAGGER_PATH")
+	if swaggerPath == "" {
+		logger.Error.Printf("SWAGGER_PATH not found")
+		return
+	}
+
+	r := routes.SetupRouter(swaggerPath)
 
 	if err := r.Run(apiPort); err != nil {
 		log.Fatalf("Error starting API: %v", err)
