e885218ff45782b4ea716a4bc8253031eb8ba018
R4yya <danil.asd24@gmail.com>
2024-10-19 21:39:16 +0300

    update logger: - create 3 loggers; - write logs in files.

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,19 +1,76 @@
 package logger
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"runtime"
 )
 
 var (
-	Info  *log.Logger
-	Error *log.Logger
+	infoLogger   *log.Logger
+	errorLogger  *log.Logger
+	debugLogger  *log.Logger
+	mainLogFile  *os.File
+	componentLog *os.File
+	debugFile    *os.File
 )
 
-func Init() {
-	Info = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+// Init initializes loggers and opens files for a specific component
+func Init(component string) {
+	var err error
 
-	Error = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
+	mainLogFile, err = os.OpenFile("main.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Не удалось открыть основной файл для логирования: %v", err)
+	}
 
-	log.Println("Logger initiated")
+	componentLog, err = os.OpenFile(fmt.Sprintf("%s.log", component), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Не удалось открыть файл для логирования компонента %s: %v", component, err)
+	}
+
+	debugFile, err = os.OpenFile(fmt.Sprintf("%s-debug.log", component), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Fatalf("Не удалось открыть файл для отладочного логирования компонента %s: %v", component, err)
+	}
+
+	infoLogger = log.New(componentLog, "INFO: ", log.Ldate|log.Ltime|log.Lmicroseconds)
+	errorLogger = log.New(mainLogFile, "ERROR: ", log.Ldate|log.Ltime|log.Lmicroseconds|log.Lshortfile)
+	debugLogger = log.New(debugFile, "DEBUG: ", log.Ldate|log.Ltime|log.Lmicroseconds)
+}
+
+// Close closes all logs files
+func Close() {
+	if mainLogFile != nil {
+		mainLogFile.Close()
+	}
+	if componentLog != nil {
+		componentLog.Close()
+	}
+	if debugFile != nil {
+		debugFile.Close()
+	}
+}
+
+// Info логирует информационное сообщение
+func Info(message string) {
+	infoLogger.Println(message)
+}
+
+// Debug logs debug message
+func Debug(message string) {
+	debugLogger.Println(message)
+}
+
+// Error logs error message then panic
+func Error(err error) {
+	pc, _, _, _ := runtime.Caller(1)
+	details := runtime.FuncForPC(pc)
+	funcName := "unknown"
+	if details != nil {
+		funcName = details.Name()
+	}
+	errorLogger.Printf("%s | %v", funcName, err)
+	panic(err)
 }
