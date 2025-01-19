a8a01a4eddc1e9134a695f1cb95ea0fc70f6c0e1
R4yya <danil.asd24@gmail.com>
2025-01-20 01:08:40 +0300

    update services/user.go - add UserStorage interface and UserService struct; - update UserService methods; - sort imports.

--- internal/services/user.go
+++ internal/services/user.go
@@ -1,31 +1,41 @@
 package services
 
 import (
+	"fmt"
+
+	intStorage "bizarre-vpn-api/internal/storage"
 	"bizarre-vpn-api/internal/storage/models"
 )
 
+type UserStorage interface {
+	GetUserByTelegramID(telegramID int64) (*models.User, error)
+	CreateUser(user *models.User) (int64, error)
+}
+
+type UserService struct {
+	storage UserStorage
+}
+
 // GetUser gets the user by Telegram ID through the repository
-func GetUser(telegramID int64) (*models.User, error) {
-	// user, err := repositories.GetUserByTelegramID(telegramID)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("failed to get user: %w", err)
-	// }
-	// return user, nil
-	return nil, nil
+func (s *UserService) GetUser(telegramID int64) (*models.User, error) {
+	user, err := s.storage.GetUserByTelegramID(telegramID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get user: %w", err)
+	}
+	return user, nil
 }
 
 // RegisterUser registers the user if it does not already exist
-func RegisterUser(user *models.User) (int64, error) {
-	// existingUser, err := repositories.GetUserByTelegramID(user.TelegramID)
-	// if err == nil && existingUser != nil {
-	// 	return 0, custom_errors.ErrUserAlreadyExists
-	// }
+func (s *UserService) RegisterUser(user *models.User) (int64, error) {
+	existingUser, err := s.storage.GetUserByTelegramID(user.TelegramID)
+	if err == nil && existingUser != nil {
+		return 0, intStorage.ErrUserAlreadyExists
+	}
 
-	// userID, err := repositories.CreateUser(user)
-	// if err != nil {
-	// 	return 0, fmt.Errorf("failed to register user: %w", err)
-	// }
+	userID, err := s.storage.CreateUser(user)
+	if err != nil {
+		return 0, fmt.Errorf("failed to register user: %w", err)
+	}
 
-	// return userID, nil
-	return 0, nil
+	return userID, nil
 }
